03ceaa5241c3cd4120e12f2fd192f90ffd32aa86
NouNoune666 <barretteamelie@gmail.com>
2025-09-09 10:23:13 -0400

    Set up details in drawing

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,20 +1,31 @@
 /**
- * Function Calls
+ * The blank page
  * Nou Noune (Amélie Barrette)
  * 
- * Working on the function calls examples and loving it.
+ * An exploration of the existential angst of a novelist
+ * who must sit down at their purple desk and confront the
+ * abyss that is the blank page of paper.
+ * 
+ * The program is non-interactive to convey the inability
+ * to get started on the project. Try typing! Just try it!
  */
 
 "use strict";
 
-
+/**
+ * Creates the canvas.
+ */
 function setup() {
-    // Once at the beginning of the program
+    // Create the canvas at a standard resolution.
     createCanvas(640, 480);
 }
 
+/**
+ * Draw's the writer's desktop and a blank piece of paper
+ */
 function draw() {
-    // Every frame
+    // The purple desktop
     background(219, 18, 244);
+    // The blank piece of paper
     rect(200, 80, 240, 320);
 }
