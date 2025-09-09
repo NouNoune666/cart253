6fe71592e14e1d3f2d4fe60f8b8581c3d6cb77a3
NouNoune666 <barretteamelie@gmail.com>
2025-09-09 15:21:41 -0400

    Created mountain and roof

--- topics/Challenges/Instruction challenge/js/script.js
+++ topics/Challenges/Instruction challenge/js/script.js
@@ -25,6 +25,8 @@ function draw() {
     background("#72bddb");
 
     drawLand();
+    drawMountain();
+    drawHouse();
 
 }
 
@@ -33,5 +35,26 @@ function drawLand() {
     fill("#8cdb72");
     noStroke();
     rect(0,230,600,170);
+    pop();
+}
+
+function drawMountain() {
+    push();
+    fill("#969696");
+    noStroke();
+    triangle(75, 230, 320, 230, 197, 75);
+    pop();
+}
+
+function drawHouse() {
+   drawRoof();
+   drawBodyHouse();
+}
+
+function drawRoof() {
+    push();
+    fill("#d62700");
+    noStroke();
+    triangle(440, 180, 510, 180, 475, 150);
     pop();
 }
