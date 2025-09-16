c91f8f6cdf198802235a002c3535bab0ef4ef0d5
NouNoune666 <barretteamelie@gmail.com>
2025-09-16 10:19:36 -0400

    Created velocity stuff!!

--- topics/Class 3/movement/js/script.js
+++ topics/Class 3/movement/js/script.js
@@ -11,6 +11,22 @@ let bird = {
     x: 120,
     y: 460,
     size: 50,
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    min.Velocity:«{
+        x: -3
+        y: -2
+    }
+    max.Velocity:«{
+        x: 3
+        y: 2
+    }
+    acceleration:{
+        x: 0.025,
+        y: - 0.05
+    }
 };
 
 /**
@@ -28,8 +44,14 @@ function draw() {
     background(0);
 
     //Move the bird
-    bird.x = bird.x + 1;
-    bird.y = bird.y - 5;
+    bird.velocity.x +=  bird.acceleration.x;
+    bird.velocity.y += bird.acceleration.y;
+
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+
+    bird.x += bird.velocity.x;
+    bird.y += bird.velocity.y;
     
 
     //Draw the bird
