ae5590875e43991d0edb70baa932e557f547a496
NouNoune666 <barretteamelie@gmail.com>
2025-09-16 16:18:45 -0400

    Changed the color of mister furious, change the sky's colour, and drew a bird

--- topics/Class 4/variables-challenge/js/script.js
+++ topics/Class 4/variables-challenge/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Mr. Furious
- * Nou Nooune
+ * Nou Noune and Ray
  *
  * A guy who becomes visibly furious!
  */
@@ -21,6 +21,29 @@ let mrFurious = {
   }
 };
 
+// Our friend the sky
+    //Colour
+    let sky= {
+        fill: {
+            r: 160,
+            g: 180,
+            b: 200
+        }
+    
+}
+
+// Our friend the bird
+let bird = {
+    //Position and size
+    x: 0,
+    y: 150,
+    size: 25,
+
+    //Colour
+    fill: 255
+}
+    
+
 /**
  * Create the canvas
  */
@@ -32,7 +55,16 @@ function setup() {
  * Draw (and update) Mr. Furious
  */
 function draw() {
-  background(160, 180, 200);
+  background(sky.fill.r, sky.fill.g, sky.fill.b);
+
+  //Make Mr.furious more and more red!
+    mrFurious.fill.g += -1
+    mrFurious.fill.b += -1
+
+ //Make the sky darker
+    sky.fill.r += -1
+    sky.fill.g += -1
+    sky.fill.b += -1
   
   // Draw Mr. Furious as a coloured circle
   push();
@@ -40,4 +72,10 @@ function draw() {
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
   ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
   pop();
+
+    // Draw the bird
+  push();
+  noStroke();   
+  fill (bird.fill);
+  ellipse(bird.x, bird.y, bird.size)
 }
