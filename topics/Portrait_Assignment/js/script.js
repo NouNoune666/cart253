27defb75915fce5aa9836f38edf8b37fba612877
NouNoune666 <barretteamelie@gmail.com>
2025-09-23 13:38:07 -0400

    Worked on portrait + changed some folders (organized them)

--- topics/Portrait_Assignment/js/script.js
+++ topics/Portrait_Assignment/js/script.js
@@ -15,18 +15,45 @@ function setup() {
     createCanvas(600, 500)
 }
 
+let starImage = undefined;
+
+function preload() {
+    // This is how you load an image!
+    // Note that loadImage() needs the PATH to your image inside your project
+    // Note that the path is CASE SENSITIVE
+    // Note that the filename is CASE SENSITIVE
+    // Note the QUOTE MARKS around the path
+    starImage = loadImage("assets/images/1_étoile.png");
+
+}
 /**
  * Creates my portrait.
  */
 
 function draw() {
-    background(0,30,60)
+    background(0,30,60);
 
-    push()
-    fill("#1ED760")
-    noStroke()
-    rect(0, 300, 400, 100)
-    pop()
+    push();
+    fill("#1ED760");
+    noStroke();
+    rect(0, 2 * height / 3, width, height / 3);
+    pop();
 
+    push();
+    imageMode(CENTER);
+    // Now the CENTER of the star image will be at (100, 50)
+    image(starImage, 100, 50);
+    pop();
 
+    push();
+    imageMode(CENTER);
+    // Now the CENTER of the star image will be at (100, 50)
+    image(starImage, 110, 70);
+    pop();
+
+    push();
+    fill("#1ED760");
+    noStroke();
+    ellipse(width/4, 300, 100, 100);
+    pop();
 }
