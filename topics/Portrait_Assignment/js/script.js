8699cc9d8cef7143437585ad12d066f473e43f95
NouNoune666 <barretteamelie@gmail.com>
2025-09-24 10:22:38 -0400

    Worked on the stars and the mountains!

--- topics/Portrait_Assignment/js/script.js
+++ topics/Portrait_Assignment/js/script.js
@@ -31,29 +31,43 @@ function preload() {
  */
 
 function draw() {
-    background(0,30,60);
+    background(0, 30, 60);
 
+    // Draws grass
     push();
     fill("#1ED760");
-    noStroke();
-    rect(0, 2 * height / 3, width, height / 3);
+    stroke("#386949ff");
+    strokeWeight(4);
+    rect(0, height * 0.85, width, height * 0.25);
     pop();
 
+    // Draws the stars
     push();
     imageMode(CENTER);
-    // Now the CENTER of the star image will be at (100, 50)
-    image(starImage, 100, 50);
+    image(starImage, width * 0.10, height * 0.10, 60, 60);
+    image(starImage, width * 0.20, height * 0.30, 60, 60);
+    image(starImage, width * 0.30, height * 0.10, 60, 60);
+    image(starImage, width * 0.40, height * 0.30, 60, 60);
+    image(starImage, width * 0.50, height * 0.10, 60, 60);
+    image(starImage, width * 0.60, height * 0.30, 60, 60);
+    image(starImage, width * 0.70, height * 0.10, 60, 60);
+    image(starImage, width * 0.80, height * 0.30, 60, 60);
+    image(starImage, width * 0.90, height * 0.10, 60, 60);
     pop();
 
+    // Draws the moutains
+    //Moutain 1
     push();
-    imageMode(CENTER);
-    // Now the CENTER of the star image will be at (100, 50)
-    image(starImage, 110, 70);
+    fill("#966638ff");
+    stroke("#5a4531ff");
+    strokeWeight(4);
+    triangle(width * 0.10, height * 0.85, width * 0.27, height * 0.5, width * 0.50, height * 0.85);
     pop();
-
+    // Moutain 2
     push();
-    fill("#1ED760");
-    noStroke();
-    ellipse(width/4, 300, 100, 100);
+    fill("#966638ff");
+    stroke("#5a4531ff");
+    strokeWeight(4);
+    triangle(width * 0.10, height * 0.85, width * 0.27, height * 0.5, width * 0.50, height * 0.85);
     pop();
 }
