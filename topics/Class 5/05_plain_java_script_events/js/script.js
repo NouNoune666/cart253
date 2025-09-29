2b55850500ee9a7837d8653626b9819b459f3f1a
NouNoune666 <barretteamelie@gmail.com>
2025-09-29 15:03:25 -0400

    Started plain java script events

---
+++ topics/Class 5/05_plain_java_script_events/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Plain Java script Events
+ * Nou Noune
+ * 
+ * Experimenting with event handling in plain Javascript.
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
