410cf16e0b6d6b30215c4a1f1b94ce8235700a3a
NouNoune666 <barretteamelie@gmail.com>
2025-10-19 17:43:16 -0400

    Did the function return values material

--- topics/Class_7/trisolaris-returns/js/script.js
+++ topics/Class_7/trisolaris-returns/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Trisolaris Returns (get it?)
- * Pippin
+ * Nou Noune
  * 
  * Draws the three suns of Trisolaris. Calculate their colour
  * based on their parameters.
@@ -25,18 +25,14 @@ function draw() {
     drawSun(500, 100, 80);
     drawSun(350, 180, 200);
     drawSun(120, 100, 160);
+
 }
 
 /**
  * Draws a Trisolarian sun
  */
 function drawSun(x, y, size) {
-    // Calculate the stroke weight of the sun based on
-    // the distance of the mouse position
-    const minWeight = 20;
-    const maxWeight = 1;
-    let d = dist(mouseX, mouseY, x, y);
-    let weight = map(d, 0, width, minWeight, maxWeight);
+    let weight = calculateStrokeWeight(x, y);
 
     push();
     strokeWeight(weight);
@@ -44,4 +40,15 @@ function drawSun(x, y, size) {
     fill("#f99736");
     ellipse(x, y, size);
     pop();
+}
+
+function calculateStrokeWeight(x, y) {
+    // Calculate the stroke weight of the sun based on
+    // the distance of the mouse position
+    const minWeight = 1;
+    const maxWeight = 20;
+    let d = dist(mouseX, mouseY, x, y);
+    let result = map(d, 0, width, minWeight, maxWeight);
+
+    return result;
 }
