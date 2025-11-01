83902789590e2d58d11115033b8650f9a0f68965
NouNoune666 <barretteamelie@gmail.com>
2025-10-31 20:19:34 -0400

    Finished star field project

--- topics/Class_9/star-field/js/script.js
+++ topics/Class_9/star-field/js/script.js
@@ -8,17 +8,49 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+// Number of stars displayed
+const NUM_STARS = 10000;
 
+/**
+ * Creates the canvas
+ */
+function setup() {
+    createCanvas(400, 400);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Creates background and stars
+ */
 function draw() {
+    background(15);
+
+    // randomSeed() allows us to make random() more predictable
+    // When we set the random seed we make it so that all the
+    // random() functions we call from here will have the same
+    // (random) return values in a sequence
+    // (Try removing it and seeing what happens!)
+    randomSeed(1);
+
+    for (let i = 0; i < NUM_STARS; i++) {
+        drawStar();
+    }
+    // Once we get here (with the for-loop done) we will have
+    // drawn NUM_STARS stars.
+}
+
+/** 
+ * Draws a star at a random position
+ */
+
+function drawStar() {
+    push();
+    const x = random(0, width);
+    const y = random(0, height);
+    const diameter = random(2, 5);
+    const color = random(10, 200);
+    fill(color);
+    ellipse(x, y, diameter);
+    pop();
 
 }
